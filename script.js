87aa1c3a155c60c77b12bf9657a5b505a844d893
TwUrS <valen_01martina@hotmail.com>
2024-11-28 20:59:49 -0300

    Version 1.1

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+const getLocalDateTime = () => {
+    const now = new Date();
+
+    let minutes = now.getMinutes();
+    let hours = now.getHours();
+
+    minutes = Math.ceil(minutes / 30) * 30;
+
+    if (minutes === 60) {
+        minutes = 0;
+        hours += 1;
+    }
+
+    return {
+        year: now.getFullYear(),
+        month: String(now.getMonth() + 1).padStart(2, "0"),
+        day: String(now.getDate()).padStart(2, "0"),
+        hours: String(hours).padStart(2, "0"),
+        minutes: String(minutes).padStart(2, "0"),
+    };
+};
+
+const localDateTime = getLocalDateTime();
+const formattedDateTime = `${localDateTime.year}-${localDateTime.month}-${localDateTime.day}T${localDateTime.hours}:${localDateTime.minutes}`;
+const datetimeInput = document.getElementById("timeField");
+datetimeInput.min = formattedDateTime;
+
+
+const validateForm = (form) => {
+    const validation = {
+        status: true,
+        message: ""
+    }
+    if (!form.name) {
+        validation.status = false
+        validation.message = "El campo Nombre y Apellido no puede estar vacio"
+    }
+
+    if (!form.age) {
+        validation.status = false
+        validation.message = "El campo edad no puede estar vacio"
+    }
+
+    if (!form.doctor) {
+        validation.status = false
+        validation.message = "El campo Doctor no puede estar vacio"
+    }
+
+    if (!form.phone) {
+        validation.status = false
+        validation.message = "El campo telefono no puede estar vacio"
+    }
+
+    if (!form.email) {
+        validation.status = false
+        validation.message = "El campo email no puede estar vacio"
+    }
+
+    if (!form.schedule) {
+        validation.status = false
+        validation.message = "El campo fecha no puede estar vacio"
+    }
+
+    return validation
+}
+
+
 const form = document.getElementById("informationForm")
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -11,6 +78,13 @@ form.addEventListener("submit", async (e) => {
         data[key] = value;
     });
 
+    const validation = validateForm(data)
+    if (!validation.status) {
+        alert(validation.message)
+        submitButton.disabled = false
+        return;
+    }
+
     try {
         const response = await fetch('http://localhost:3000/saveQuestionForm', {
             method: 'POST',
@@ -20,12 +94,15 @@ form.addEventListener("submit", async (e) => {
             body: JSON.stringify(data)
         });
 
+        const responseData = await response.json();
         if (!response.ok) {
-            throw new Error('Error en la solicitud');
+            alert(responseData.message)
+            submitButton.disabled = false
+            return
         }
 
         e.target.reset()
-        alert("Su solicitud ha sido enviada con éxito");
+        alert(responseData.message);
     }
     catch (error) {
         console.error("Hubo un error en la solicitud: ", error);
